787d7ae5cca016c54037dd3abd04c2b45a4e9f44
grzesiek1362 <leslie.oladele@gmail.com>
2021-11-13 15:06:06 +0100

    #2 - creating product-list-component

---
+++ src/app/product-list/product-list.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import {Product} from "../product";
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent implements OnInit {
+
+  products: Product[] | undefined;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
